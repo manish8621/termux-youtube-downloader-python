f503d5d61c269031634dbe4dddcb5e8f5ddcf093
manish <43578519+manish8621@users.noreply.github.com>
2021-05-17 08:16:01 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+mkdir data/data/com.termux/files/home/bin
+echo "created a folder..."
+cp termux-url-opener data/data/com.termux/files/home/bin
+echo "copied a termux-url-opener to bin"
+echo "Successfully Installed !"
+echo "run the yt.py to download youtube videos"
+echo "share the youtube video from Youtube app to Termux to download directly"
+echo "have fun ;)"
